cc3ad1b4c28a2467d68eb4f587a3ef5c432a6069
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-23 11:44:04 -0300

    conectando api do github

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from 'react'
+import api from './api'
+
 function App() {
-  return <div>OLÁÁÁÁ</div>
+  const [users, setUsers] = useState()
+
+  useEffect(() => {
+    api
+      .get('/users')
+      .then((response) => setUsers(response.data))
+      .catch((err) => {
+        console.error('ops! ocorreu um erro' + err)
+      })
+  }, [])
+
+  if (!users) return null
+  return (
+    <div>
+      {users.map((user) => (
+        <p key={user.id} className="flex flex-col mb-4">
+          {user.login}
+        </p>
+      ))}
+    </div>
+  )
 }
 
 export default App
